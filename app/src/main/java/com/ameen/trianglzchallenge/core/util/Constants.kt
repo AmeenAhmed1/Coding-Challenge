c219c9dcdd899aef501f1eecc9d8d1c11d84a106
AmeenAhmed1 <ameen.mobiledev@gmail.com>
2022-06-25 23:24:44 +0200

    Change Image Size to (w500)

--- app/src/main/java/com/ameen/trianglzchallenge/core/util/Constants.kt
+++ app/src/main/java/com/ameen/trianglzchallenge/core/util/Constants.kt
@@ -3,7 +3,7 @@ package com.ameen.trianglzchallenge.core.util
 const val PAGE_SIZE = 20
 const val INITIAL_LOADING_PAGE_NUMBER = 1
 const val BASE_URL = "https://api.themoviedb.org/3/"
-const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
+const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 const val API_KEY = "cf2a0e44ebd0f153e44c7a54007b3f1c"
 
 const val RECYCLER_VIEW_GRID_SPAN_SIZE = 2
